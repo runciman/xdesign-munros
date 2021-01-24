5c624a6a34ad3f448404d61e90c84601bc458ee5
runciman <scott.runciman@me.com>
2021-01-24 15:41:53 +0000

    Adding basic, albeit clumsy, parsing from CSV to model

--- xDesign Munros/xDesign Munros/Munro.swift
+++ xDesign Munros/xDesign Munros/Munro.swift
@@ -10,10 +10,10 @@ import Foundation
 
 struct Munro {
     
-    enum MunroClassification {
-        case munro
-        case top
-        case none
+    enum MunroClassification: String {
+        case munro = "MUN"
+        case top = "TOP"
+        case none = ""
     }
     
     struct EraClassification {
@@ -30,24 +30,101 @@ struct Munro {
         let post1997: MunroClassification
     }
     
-    let runningNumber: Int
-    let doBIHNumber: Int
-    let streetmapURL: URL
-    let geographURL: URL
-    let hillBaggingURL: URL
-    let name: String
-    let smcSection: Int
-    let rhbSection: String
-    let section: Double
-    let heightInMetres: Int
-    let heightInFeet: Int
-    let mapOneFiftyScale: String
-    let mapOneTwentyFiveScale: String
-    let gridReference: String
-    let gridReferenceXY: String
-    let xCoordinate: Int
-    let yCoordinate: Int
+    var runningNumber: Int
+    var doBIHNumber: Int
+    var streetmapURL: URL
+    var geographURL: URL
+    var hillBaggingURL: URL
+    var name: String
+    var smcSection: Int
+    var rhbSection: String
+    var section: Double
+    var heightInMetres: Int
+    var heightInFeet: Int
+    var mapOneFiftyScale: String
+    var mapOneTwentyFiveScale: String
+    var gridReference: String
+    var gridReferenceXY: String
+    var xCoordinate: Int
+    var yCoordinate: Int
+    var eraClassification: EraClassification
+    var comments: String
+}
 
-    let eraClassification: EraClassification
-    let comments: String
+extension Munro {
+    
+    enum Error: Swift.Error {
+        case nonCSVString
+        case missingEntries
+        case typeMismatch(entryName: String)
+    }
+    
+    init?(_ csvEntry: String) throws {
+        let split = csvEntry.split(separator: ",")
+        guard split.count > 0 else {
+            throw Munro.Error.nonCSVString
+        }
+        
+        try self.init(split)
+    }
+    
+    init?<T>(_ entries: [T]) throws where T: StringProtocol {
+        guard entries.count == 15 else {
+            throw Munro.Error.missingEntries
+        }
+        
+        func value<T: StringProtocol, V>(value: T, as type: V.Type) throws -> V {
+            
+            let finalValue: V
+            switch V.self {
+            case is String.Type:
+                finalValue = String(value) as! V
+            case is Int.Type:
+                finalValue = Int(value) as! V
+            case is Double.Type:
+                finalValue = Double(value) as! V
+            case is URL.Type:
+                finalValue = URL(string: String(value)) as! V
+            default:
+                throw Munro.Error.missingEntries
+            }
+            
+            return finalValue
+        }
+        
+        
+        runningNumber = try value(value: entries[0], as: Int.self)
+        doBIHNumber = try value(value: entries[1], as: Int.self)
+        streetmapURL = try value(value: entries[2], as: URL.self)
+        geographURL = try value(value: entries[3], as: URL.self)
+        hillBaggingURL = try value(value: entries[4], as: URL.self)
+        name = try value(value: entries[5], as: String.self)
+        smcSection = try value(value: entries[6], as: Int.self)
+        rhbSection = try value(value: entries[7], as: String.self)
+        section = try value(value: entries[8], as: Double.self)
+        heightInMetres = try value(value: entries[9], as: Int.self)
+        heightInFeet = try value(value: entries[10], as: Int.self)
+        mapOneFiftyScale = try value(value: entries[11], as: String.self)
+        mapOneTwentyFiveScale = try value(value: entries[12], as: String.self)
+        gridReference = try value(value: entries[13], as: String.self)
+        gridReferenceXY = try value(value: entries[14], as: String.self)
+        xCoordinate = try value(value: entries[15], as: Int.self)
+        yCoordinate = try value(value: entries[16], as: Int.self)
+        
+        let year1891 = MunroClassification(rawValue: String(entries[17])) ?? MunroClassification.none
+        let year1921 = MunroClassification(rawValue: String(entries[18])) ?? MunroClassification.none
+        let year1933 = MunroClassification(rawValue: String(entries[19])) ?? MunroClassification.none
+        let year1953 = MunroClassification(rawValue: String(entries[20])) ?? MunroClassification.none
+        let year1969 = MunroClassification(rawValue: String(entries[21])) ?? MunroClassification.none
+        let year1974 = MunroClassification(rawValue: String(entries[22])) ?? MunroClassification.none
+        let year1981 = MunroClassification(rawValue: String(entries[23])) ?? MunroClassification.none
+        let year1984 = MunroClassification(rawValue: String(entries[24])) ?? MunroClassification.none
+        let year1990 = MunroClassification(rawValue: String(entries[25])) ?? MunroClassification.none
+        let year1997 = MunroClassification(rawValue: String(entries[26])) ?? MunroClassification.none
+        let post1997 = MunroClassification(rawValue: String(entries[27])) ?? MunroClassification.none
+        
+        eraClassification = EraClassification(year1891: year1891, year1921: year1921, year1933: year1933, year1953: year1953, year1969: year1969, year1974: year1974, year1981: year1981, year1984: year1984, year1990: year1990, year1997: year1997, post1997: post1997)
+        
+        comments = try value(value: entries[28], as: String.self)
+    }
 }
