3eb1897d831abf351368cea17e25a48a1573e408
runciman <scott.runciman@me.com>
2021-01-24 14:18:48 +0000

    Adding Xcode project for framework

---
+++ xDesign Munros/xDesign Munros/xDesign_Munros.h
@@ -0,0 +1,18 @@
+//
+//  xDesign_Munros.h
+//  xDesign Munros
+//
+//  Created by Scott Runciman on 24/01/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for xDesign_Munros.
+FOUNDATION_EXPORT double xDesign_MunrosVersionNumber;
+
+//! Project version string for xDesign_Munros.
+FOUNDATION_EXPORT const unsigned char xDesign_MunrosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <xDesign_Munros/PublicHeader.h>
+
+
